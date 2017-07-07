ff22b184c96d56108f8176f4011b437d25bb7ac8
Renato Cardoso <re2005@gmail.com>
2017-07-07 20:59:24 +0200

    refactor(geth-contract-manager.service): add valid contract addresses

--- src/app/services/geth-contract-manager/geth-contract-manager.service.ts
+++ src/app/services/geth-contract-manager/geth-contract-manager.service.ts
@@ -4,9 +4,11 @@ import {Injectable} from '@angular/core';
 export class GethContractManagerService {
 
     private _contracts = [
-        '0x19e75b7760b1b8e708c085ab83ab7b62db126578',
-        '0x19e75b7760b1b8e708c085ab83ab7b62db126577',
-        '0x19e75b7760b1b8e708c085ab83ab7b62db126576'
+        '0xaa2179259BaAf3229EF598306306bf6b8A38ec52',
+        '0x83E90B9bC5929d3403ECB621fC913f3d9A3d1060',
+        '0x8451220218F52FdCc4c3f06Ef019ee82c58C37A5',
+        '0xD1498F1c4AAafcd1eAA1b6e3594a37C7Fd7D0909',
+        '0xE1B5BeaC131Cb9Dd9F783F859939F1BDf8B5857b'
     ];
 
     constructor() {
