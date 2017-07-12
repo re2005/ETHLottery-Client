3441779e9d63da6287e3ddfd85ec3015b7a9aa08
Renato Cardoso <re2005@gmail.com>
2017-07-12 23:09:54 +0200

    refactor(): add contract & rise min gas

--- src/app/services/geth-contract-manager/contracts.ts
+++ src/app/services/geth-contract-manager/contracts.ts
@@ -1,5 +1,7 @@
 export const contracts = {
     apiData: [
+        '0xb7953cf6475d1094517934bf94b2F4442C0224f7',
+        '0x77566235f72025cbdeCe35CAec98C6983492056c',
         '0x5d454F71354a5A84D03Fd8d0DADFAe320c25CCA4',
         '0x3b79664E14B4Da79fb58B9Ca4BCFd6905f53f67C',
     ]
