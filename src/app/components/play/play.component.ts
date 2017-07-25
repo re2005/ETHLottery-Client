647f6d504517f497d86d238cdc73e663536a8d71
Renato Cardoso <re2005@gmail.com>
2017-07-25 21:35:32 +0200

    refactor(): adjust error play messages & lottery list

--- src/app/components/play/play.component.ts
+++ src/app/components/play/play.component.ts
@@ -11,6 +11,7 @@ export class PlayComponent implements OnInit {
 
     @Input() play: any;
     public isBetInvalid: boolean;
+    public playErrorMessage: any;
     public bets = ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     /**
@@ -61,8 +62,7 @@ export class PlayComponent implements OnInit {
      * @param errorMessage
      */
     onPlayError(errorMessage) {
-        errorMessage = this._cleanErrorMessage(errorMessage);
-        alert(errorMessage);
+        this.playErrorMessage = this._cleanErrorMessage(errorMessage);
     }
 
     /**
