f4683b9a21e5eaf18dc0648c5173dbef1c595266
Renato Cardoso <re2005@gmail.com>
2017-08-02 18:21:18 +0200

    refactor(): update visuals remove unused service/directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import {GethContractService} from './services/geth-contract/geth-contract.servic
 import {GethContractManagerService} from './services/geth-contract-manager/geth-contract-manager.service';
 import {StorageService} from './services/storage/storage.service'
 import {HttpModule, JsonpModule} from '@angular/http';
-import {ApiStateService} from './services/api-state/api-state.service';
 import {AccountService} from './services/account/account.service';
 import {PlayComponent} from './components/play/play.component';
 import {LoadingComponent} from './components/loading/loading.component';
 import {PlayService} from './services/play/play.service';
 import {IconGeneratorService} from './services/icon-generator/icon-generator.service';
 import {LotteryIconComponent} from './components/lottery-icon/lottery-icon.component';
-import {ScaleDirective} from './directives/scale.directive';
 import {ServerSocketService} from './services/server-socket/server-socket.service';
 import {TutorialComponent} from './components/tutorial/tutorial.component';
 
@@ -24,7 +22,6 @@ import {TutorialComponent} from './components/tutorial/tutorial.component';
         PlayComponent,
         LoadingComponent,
         LotteryIconComponent,
-        ScaleDirective,
         TutorialComponent
     ],
     imports: [
@@ -36,7 +33,6 @@ import {TutorialComponent} from './components/tutorial/tutorial.component';
         GethContractService,
         GethContractManagerService,
         StorageService,
-        ApiStateService,
         AccountService,
         PlayService,
         IconGeneratorService,
--- src/app/services/api-state/api-state.d.ts
+++
@@ -1,3 +0,0 @@
-export interface ApiStateInterface {
-    isLoaded: boolean;
-}
