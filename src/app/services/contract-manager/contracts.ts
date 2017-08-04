60959d6dc5e22985d492623253f33ee56fe4cc6b
Renato Cardoso <re2005@gmail.com>
2017-08-04 22:45:45 +0200

    feat(): add slack invitation

--- src/app/services/contract-manager/contracts.ts
+++ src/app/services/contract-manager/contracts.ts
@@ -8,6 +8,11 @@ export const contracts = {
         '0xe440f3ffd094779204bffe7f20d063e6f6fd606c',
         '0xa9bdf7bf910ce0af11372adc062f54bec7a9ae9f',
         '0x8e85aedd658f9a3a73a7268b71b87603fc6ee994',
-        '0x698bee2d2c506297ad33cb31305e2e7f56f8611c'
+        '0x698bee2d2c506297ad33cb31305e2e7f56f8611c',
+        // '0x1e461eB1423Ea3D2e835B6B0c7De7BEA258E4fb4',
+        // '0x005831000055f22e78a6Aa9341745daa5860F388',
+        // '0xe526d87AFD6fe568377DB11Fb8744f0db6f01B05',
+        // '0x132010f214A2b72A6C3E4691a551BBf935ab66e2',
+        // '0x95E44f576a37069FE5f181EE4dA6FAc727dF5D48',
     ]
 }
